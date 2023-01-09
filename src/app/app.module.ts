8beb1ffc57a43506f697bf2884acfb8e9fc73233
Abbas4tech <shaikhabbas54@gmail.com>
2023-01-09 11:53:42 +0530

    feat: added pwa configs and manifests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
 import { StoreModule } from '@ngrx/store';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,6 +16,12 @@ import { StoreModule } from '@ngrx/store';
         SharedModule,
         BrowserAnimationsModule,
         StoreModule.forRoot(),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: true,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent],
