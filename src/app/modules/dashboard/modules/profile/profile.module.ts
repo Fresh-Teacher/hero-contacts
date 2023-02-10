e1e2dc9f5b4c47a79e89cda85da8f4ec9ad80d4c
Abbas4tech <shaikhabbas54@gmail.com>
2023-02-10 19:36:03 +0530

    feat: Profile name update and minor ui improvments

--- src/app/modules/dashboard/modules/profile/profile.module.ts
+++ src/app/modules/dashboard/modules/profile/profile.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { IndexProfileScreen } from './screen/index.screen';
 
 @NgModule({
     declarations: [IndexProfileScreen],
-    imports: [CommonModule, ProfileRoutingModule],
+    imports: [
+        CommonModule,
+        ProfileRoutingModule,
+        ReactiveFormsModule,
+        SharedModule,
+    ],
     exports: [],
 })
 export class ProfileModule {}
--- src/app/modules/dashboard/modules/profile/screen/index.screen.ts
+++ src/app/modules/dashboard/modules/profile/screen/index.screen.ts
@@ -1,3 +1,4 @@
+import { fadeInOut } from './../../../../shared/animations/shared.animations';
 import { Component, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { updateProfile, User } from '@angular/fire/auth';
@@ -8,9 +9,18 @@ import {
 import { TStoFix } from 'src/app/types/common-types';
 import { ToastService } from 'src/app/services/toaster.service';
 import { Subscription } from 'rxjs';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+} from '@angular/forms';
+import { Location } from '@angular/common';
+import { noSpace } from '../../contacts/validators/validators';
 @Component({
     selector: 'profile',
     templateUrl: './index.screen.html',
+    animations: [fadeInOut],
 })
 export class IndexProfileScreen implements OnDestroy {
     task: AngularFireUploadTask;
@@ -19,14 +29,24 @@ export class IndexProfileScreen implements OnDestroy {
     percentage: number;
     user: User;
 
+    updateForm: FormGroup;
+    isLoading: boolean;
+
     constructor(
         private _auth: AuthService,
         private _fireStorage: AngularFireStorage,
-        private _toastr: ToastService
+        private _toastr: ToastService,
+        private _fb: FormBuilder,
+        private _location: Location
     ) {
         this.suscriptions.push(
-            this._auth.user.subscribe((user) => (this.user = user))
+            this._auth.user.subscribe((user) => {
+                this.user = user;
+            })
         );
+        this.updateForm = this._fb.group({
+            username: new FormControl(this.user.displayName, [noSpace]),
+        });
     }
     async uploadprofile(event: TStoFix) {
         try {
@@ -50,6 +70,32 @@ export class IndexProfileScreen implements OnDestroy {
         }
     }
 
+    async submitForm(): Promise<void> {
+        if (!this.updateForm.valid) {
+            return;
+        }
+        try {
+            this.isLoading = true;
+            const { username } = this.updateForm.value;
+            await updateProfile(this._auth.user.value, {
+                displayName: username,
+            });
+            this._toastr.success('Profile Updated Successfully');
+            this.isLoading = false;
+        } catch (err) {
+            console.error(err);
+            this._toastr.error('Unable to update profile!');
+            this.isLoading = false;
+        }
+    }
+
+    back(): void {
+        this._location.back();
+    }
+
+    get username(): AbstractControl {
+        return this.updateForm.get('username');
+    }
     ngOnDestroy(): void {
         this.suscriptions.forEach((sub) => sub.unsubscribe());
     }
